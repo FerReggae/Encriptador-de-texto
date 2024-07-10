b479e3e1c083eed668f53b92b0aca405c6f92dc9
Fernando Ortiz <ortiz.valencia@hotmail.com>
2024-07-09 22:40:15 -0600

    Diseñp de la aplicaión con css y responsive desing junto con la funcionalidad de encriptar un texto

---
+++ index.js
@@ -0,0 +1,30 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    //let tituloMensaje = document.getElementById("titulo-mensaje"); 
+    //let parrafo = document.getElementById("parrafo");
+
+    /*Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+    La letra "e" es convertida para "enter"
+    La letra "i" es convertida para "imes"
+    La letra "a" es convertida para "ai"
+    La letra "o" es convertida para "ober"
+    La letra "u" es convertida para "ufat"*/
+
+    let textoCifrado = texto
+        .replace(/e/gi, "enter") /*remplazo global "gi"*/
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+
+    if (document.getElementById("texto").value.length != 0) { /* "!=0" si es diferente de cero se puede cifrar*/
+        document.getElementById("texto").value = textoCifrado;  
+        tituloMensaje.textContent = "Texto encriptado con éxito"; /*texto contenido "textContent" */       
+        parrafo.textContent = "";
+    } else {
+        alert("Debes ingresar algún texto");
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deceas encriptar o desencriptar";
+    }
+}
